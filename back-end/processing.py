ab514dad06ad54639d41295228c3cfa921357a11
Liu Yiqing <liu.yiqing@dhs.edu.sg>
2024-06-13 19:37:24 +0800

    Update processing.py

--- back-end/processing.py
+++ back-end/processing.py
@@ -1,5 +1,3 @@
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, Normalizer
-from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.manifold import TSNE
 from sklearn.metrics import mean_squared_error
 
