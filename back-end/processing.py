39eb2f1b803487658eb3288e0f675ea9b15976c9
Silver <83893730+ExoSteel@users.noreply.github.com>
2024-06-13 16:30:33 +0800

    Reorganising

--- back-end/processing.py
+++ back-end/processing.py
@@ -1,19 +1,19 @@
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, Normalizer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.manifold import TSNE
-from sklearn.metrics import mean
+from sklearn.metrics import mean_squared_error
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-distributor_df = pd.read_csv("distributor.csv")
+distributor_df = pd.read_csv("datasets\distributors.csv")
 
 from sklearn.preprocessing import OrdinalEncoder
 encoder = OrdinalEncoder(categories = [["Restaurant/Food outlet", "Supermarket"]])
 distributor_df[["distributor_Type"]] = encoder.fit_transform(distributor_df[["distributor_Type"]])
 
-recipient_df = pd.read_csv("recipients.csv")
+recipient_df = pd.read_csv("datasets/recipients.csv")
 
 rec_catgry = [["Old Folks Home", "Food shelter", "Homeless shelter", "Animal shelter"]]
 
