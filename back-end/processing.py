c4bda17097d88399b2e6e34d7646edd43f160988
Liu Yiqing <liu.yiqing@dhs.edu.sg>
2024-06-13 14:43:50 +0800

    Update processing.py

--- back-end/processing.py
+++ back-end/processing.py
@@ -2,3 +2,20 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler, Normalizer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.manifold import TSNE
 from sklearn.metrics import mean
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+distributor_df = pd.read_csv("distributor.csv")
+
+from sklearn.preprocessing import OrdinalEncoder
+encoder = OrdinalEncoder(categories = [["Restaurant/Food outlet", "Supermarket"]])
+distributor_df[["distributor_Type"]] = encoder.fit_transform(distributor_df[["distributor_Type"]])
+
+recipient_df = pd.read_csv("recipients.csv")
+
+rec_catgry = [["Old Folks Home", "Food shelter", "Homeless shelter", "Animal shelter"]]
+
+encoder = OrdinalEncoder(categories = rec_catgry)
+recipient_df[["recipient_Type"]] = encoder.fit_transform(recipient_df[["recipient_Type"]])
