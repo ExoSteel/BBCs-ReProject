682d677228420e763256773b1f09c3b27516c020
Silver <83893730+ExoSteel@users.noreply.github.com>
2024-06-13 21:47:07 +0800

    login

--- pages/login.py
+++ pages/login.py
@@ -6,8 +6,23 @@ if 'page' not in st.session_state:
 
 st.set_page_config(page_title="Log In", page_icon="smile.png", layout="wide")
 
-if st.session_state['page'] == 1:
-    st.Header("Login Page")
+if st.session_state['page'] == 1: # Log In page
+    st.header("Login Page")
+    userName = st.text_input("Organisation Name")
+    password = st.text_input("Password")
+
+    col = st.columns(2)
+    with col[0]:
+        if st.button("Sign Up"):
+            st.session_state["page"] = 2
+            st.rerun()
+    with col[1]:
+        if st.button("Confirm"):
+            st.session_state["page"] = 3
+            st.rerun()
+    
+elif st.session_state["page"] == 2: # Sign Up page
+    st.write("Sign Up")
     userType = st.selectbox(
         "Are you a distributor or a recipient?",
         ("Distributor", "Recipient"))
@@ -20,6 +35,8 @@ if st.session_state['page'] == 1:
             ("Old Folk's Home", "Homeless Shelter", "Animal Shelter"))
         address = st.text_input("Address")
         street = st.text_input("Street")
+        opHour = st.text_input("Opening Hours/Availability")
+        noOfRec = st.slider("Number of beneficiaries", 1, 100)
         
 
     elif userType == "Distributor":
@@ -34,50 +51,13 @@ if st.session_state['page'] == 1:
         opHour = st.text_input("Opening Hours/Availability")
     st.write()
     if st.button("Sign Up"):
-        st.session_state["page"] = 2
-        st.rerun()
-elif st.session_state["page"] == 2:
-    st.write("This is page two")
-    if st.button("Previous page"):
-        st.session_state["page"] = 1
+        st.session_state["page"] = 3
         st.rerun()
 
+# Main Page
+elif st.session_state['page'] == 3:
+    pass
 
 #Creating offers/requests
-if sidebarChoice == "Create Offer/Request":
-    st.header("Submitting a ticket")
-    userType = st.selectbox(
-        "Are you a distributor or a recipient?",
-        ("Distributor", "Recipient"))
-    userName = st.text_input("Enter username")
-    st.write("Do not have an account? Create one in the profile tab")
-
-    establishment = st.selectbox(
-        "Type of organisation",
-    ("Hotels/Restaurants","Supermarkets"))
-    if userType == "Recipient":
-        noOfRec = st.slider("Number of beneficiaries", 1, 50)
-
-    elif userType == "Distributor":
-        if establishment == "Hotels/Restaurants":
-            foodName = st.text_input("Enter name of food")
-            foodType = st.selectbox(
-                "What are you donating",
-            ("raw ingredients","cooked/baked goods"))
-
-            if foodType == "raw ingredients":
-                ingredient = st.selectbox(
-                    "Enter ingredients you will be donating",
-                ("Veg","Fruit","Meat","Wheat","Dairy","Others"))
-
-            elif foodType == "cooked/baked goods":
-                allergens = st.text_input("Enter any common allergens in the dish")
-
-        elif establishment == "Supermarkets":
-            foodName = st.text_input("Enter name of food")
-
-        expire = st.text_input("Enter expiration date/best by date")
-        qty = st.slider("Quantity of food supplied",1,50)
-        fridge = st.checkbox("Requires refridgeration?")
-
-    st.button("Submit ticket")
+elif st.session_state['page'] == 4:
+    pass
